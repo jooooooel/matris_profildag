8e347491fde9a8c96022115302576eb040611c3e
realmatotes <mathias.olausson@lbs.se>
2021-09-22 21:34:03 +0200

    Added matrix drawing property

---
+++ Matrix/MatrixDrawer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class MatrixDrawer
+    {
+        /// <summary>
+        /// Ritar ut matrisen på den nuvarande platsen där markören är
+        /// </summary>
+        /// <param name="m00">Det första elementet (rad 1, kolumn 1)</param>
+        /// <param name="m01">Det andra elementet (rad 1, kolumn 2)</param>
+        /// <param name="m10">Det tredje elementet (rad 2, kolumn 1)</param>
+        /// <param name="m11">Det fjärde elementet (rad 2, kolumn 2)</param>
+        public static void Draw2x2Matrix(float m00, float m01, float m10, float m11)
+        {
+            const int VALUE_LENGTH = 4;
+            const int SPACER = 2;
+            const int N = VALUE_LENGTH * 2 + SPACER + 1;
+
+            const string FORMAT = "{0:0.##}";
+
+            int cursorX = Console.CursorLeft;
+            int cursorY = Console.CursorTop;
+
+            Console.Write("┌");
+
+            Console.SetCursorPosition(cursorX + N, cursorY++);
+            Console.Write("┐");
+
+            Console.SetCursorPosition(cursorX, cursorY);
+            Console.Write("│");
+
+            Console.Write(string.Format("{0,-4}  ", string.Format(FORMAT, m00)));
+            Console.Write(string.Format("{0,4}", string.Format(FORMAT, m01)));
+
+            Console.SetCursorPosition(cursorX + N, cursorY++);
+            Console.Write("│");
+
+            Console.SetCursorPosition(cursorX, cursorY);
+            Console.Write("│");
+
+            Console.SetCursorPosition(cursorX + N, cursorY++);
+            Console.Write("│");
+
+            Console.SetCursorPosition(cursorX, cursorY++);
+            Console.Write("│");
+
+            Console.Write(string.Format("{0,-4}  ", string.Format(FORMAT, m10)));
+            Console.Write(string.Format("{0,4}", string.Format(FORMAT, m11)));
+            Console.Write("│");
+
+            Console.SetCursorPosition(cursorX, cursorY);
+            Console.Write("└");
+
+            Console.SetCursorPosition(cursorX + N, cursorY);
+            Console.Write("┘");
+        }
+
+        public static void Draw2x2Matrix(float[,] matrix)
+        {
+            Draw2x2Matrix(matrix[0, 0], matrix[0, 1], matrix[1, 0], matrix[1, 1]);
+        }
+    }
+}
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -10,6 +10,8 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
+            MatrixDrawer.Draw2x2Matrix(1.01f, 0.00f, 0.00f, 1);
+            Console.ReadKey();
         }
     }
 }
